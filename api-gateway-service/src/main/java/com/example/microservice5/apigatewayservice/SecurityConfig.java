3e8af9ec6d7868f31cf35e632b16e7beea065e8f
Gagan Sharma <gagansharma2717@gmail.com>
2022-04-19 23:59:39 +0530

    Integrated OAuth2 Login using KeyCloak

---
+++ api-gateway-service/src/main/java/com/example/microservice5/apigatewayservice/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.microservice5.apigatewayservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+//   This one is fpr browser based login
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+//        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+//                .oauth2Login(withDefaults());
+//        http.csrf().disable();
+//        return http.build();
+//    }
+
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                        .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        http.csrf().disable();
+        return http.build();
+    }
+}
