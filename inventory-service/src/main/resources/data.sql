9621858625b7790b1ca78d48e3ba2215f0c2bcd2
Gagan Sharma <gagansharma2717@gmail.com>
2022-04-28 13:13:39 +0530

    Updated Some Dependencies

--- inventory-service/src/main/resources/data.sql
+++ inventory-service/src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO `inventory-service`.`inventory`(`id`,`sku_code`,`stock`)VALUES ('1',"IPHONE_12_RED",'100');
-INSERT INTO `inventory-service`.`inventory`(`id`,`sku_code`,`stock`)VALUES ('2',"IPHONE_12_GREY",'10');
+INSERT INTO `inventory-service`.`inventory`(`id`,`sku_code`,`stock`)VALUES ('4','IPHONE_12_RED','100');
+INSERT INTO `inventory-service`.`inventory`(`id`,`sku_code`,`stock`)VALUES ('5','IPHONE_12_GREY','0');
+
+select * from `inventory-service`.`inventory`;
